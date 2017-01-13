4ced37b007a3be99be097e10602353bf1a2cbb6d
Richard <hello@noxan.net>
2017-01-13 21:07:08 +0100

    split all git related functions to lib/git.js file

--- bin/index.js
+++ bin/index.js
@@ -2,40 +2,14 @@
 
 const process = require('process');
 const path = require('path');
-const git = require('git-state');
-const gittags = require('git-semver-tags');
 const semver = require('semver');
 
 const pwd = process.cwd();
 
-git.isGit(pwd, function (exists) {
-  if (!exists) {
-    throw new Error('Needs to be called within a git repository.');
-  }
+const git = require('../lib/git');
 
-  git.check(pwd, function (err, result) {
-    if (err) {
-      throw err;
-    }
-
-    const dirty = result.ahead > 0 || result.dirty > 0;
-
-    console.log('dirty', dirty);
-  });
-});
-
-gittags(function (err, tags) {
-  if (err) {
-    throw err;
-  }
-
-  if (tags.length < 1) {
-    throw new Error('No git releases so far.');
-  }
-
-  const latestTag = tags[0];
-  console.log('git', latestTag);
-});
+git.dirty();
+git.version();
 
 const packageFile = require(path.join(pwd, 'package.json'));
 
