dec6c06d620a3cceefa132ae7a76f60ce86fa59d
Richard <hello@noxan.net>
2017-01-13 20:26:44 +0100

    install git-state and detect dirty states

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,26 @@
 
 const process = require('process');
 const path = require('path');
+const git = require('git-state');
 const semver = require('semver');
 
 const pwd = process.cwd();
+
+git.isGit(pwd, function (exists) {
+  if (!exists) {
+    throw new Error('Needs to be called within a git repository.');
+  }
+
+  git.check(pwd, function (err, result) {
+    if (err) {
+      throw err;
+    }
+
+    const dirty = result.ahead > 0 || result.dirty > 0;
+
+    console.log('dirty', dirty);
+  });
+});
 
 const packageFile = require(path.join(pwd, 'package.json'));
 
