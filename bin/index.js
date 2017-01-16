65aecf3821e8903f42b770640ecd2c53639a2c3e
Richard <hello@noxan.net>
2017-01-16 18:45:01 +0100

    restructure bin to parse arguments first, perform proper dirty check and do the rest afterwards, close #1, close #3

--- bin/index.js
+++ bin/index.js
@@ -7,15 +7,6 @@ const git = require('../lib/git');
 const npm = require('../lib/npm');
 const manifest = require('../lib/manifest');
 
-git.dirty().then(
-  dirty => console.log('dirty', dirty)
-);
-git.version().then(
-  version => console.log('git', version)
-);
-
-const npmVersion = npm.version();
-console.log('npm', npmVersion.raw);
 
 
 const semver = require('semver');
@@ -33,10 +24,25 @@ const flags = args.parse(process.argv);
 
 const identifier = flags.major ? 'major' : flags.minor ? 'minor' : 'patch';
 
-const nextVersion = getNextVersion(npmVersion, identifier);
-console.log('next', identifier, 'is', nextVersion.raw);
+git.dirty().then(
+  dirty => {
+    if (dirty) {
+      throw new Error('Git is dirty, please commit your changes before creating a release.');
+    }
 
-if (!flags.dryRun) {
-  manifest.updateVersion(nextVersion);
-  git.update(nextVersion);
-}
+    git.version().then(
+      version => console.log('git', version)
+    );
+
+    const npmVersion = npm.version();
+    console.log('npm', npmVersion.raw);
+
+    const nextVersion = getNextVersion(npmVersion, identifier);
+    console.log('next', identifier, 'is', nextVersion.raw);
+
+    if (!flags.dryRun) {
+      manifest.updateVersion(nextVersion);
+      git.update(nextVersion);
+    }
+  }
+);
